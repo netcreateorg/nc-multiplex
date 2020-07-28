a91dd6d8b5ef8c6f4b41e5191595878ea9ab1493
Ben Loh <ben@inquirium.net>
2020-07-28 13:32:19 -0700

    Fix grammar

--- nc-multiplex.js
+++ nc-multiplex.js
@@ -437,8 +437,8 @@ function RenderNewGraphForm() {
         }
       </style>
       <p>Add a template file with the same name to the /runtime folder.  Otherwise, the system 
-      make a copy of the default template.</p>
-      <label>Enter a short alphanumeric name for the database.  No spaces, no punctuation.
+      will generate a copy from the default template.</p>
+      <label>Enter a short alphanumeric name for the database.  No spaces, no punctuation.<br/>
         <form>
           <input id="dbname" placeholder="Enter new graph name" required pattern="^[a-zA-Z0-9]+$"> 
           <button onclick="OpenGraph()">Create New Graph</button>
