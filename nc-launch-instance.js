a889cdf8d5f1d14656dbb6578e4c36359741894e
DSri Seah <david@davidseah.com>
2023-07-25 16:16:30 -0400

    Update `nc-multiplex` to use dynamic paths from `nc-launch-config`; other fixes
    fix (1) rename `nc-start` to more functional name `nc-launch-instance`
    fix (2) linter format to code standards

--- nc-launch-instance.js
+++ nc-launch-instance.js
@@ -1,55 +1,55 @@
 #!/usr/bin/env node
 /**
- *  nc-start.js
+ *  nc-launch-instance.js
  *
  *  This shell script will start NetCreate
  *  brunch server directly, bypassing nc.js
- *  
+ *
  *  It is used in multi-db environments to start up
  *  multiple nc.js instances.
- * 
- *  Generally it is not called directly, but 
+ *
+ *  Generally it is not called directly, but
  *  is called by `nc-multiplex.js`.
  *
  *  It assumes that the app has been built.
  *
  */
 
-const shell = require("shelljs");
-const NCUTILS = require("./modules/nc-utils.js");
+const shell = require('shelljs');
+const NCUTILS = require('./modules/nc-utils');
+const { NC_SERVER_PATH, NC_CONFIG_PATH } = require('./nc-launch-config');
 
-const pathToNetcreate = './netcreate-itest';
+console.log('***', NC_SERVER_PATH, NC_CONFIG_PATH);
 
-const PRE = '.......nc-start: ';
+const PRE = '...nc-launch-instance:';
 
 function writeConfig(data) {
   let script = NCUTILS.GetNCConfig(data);
-shell.ShellString(script).to(`${pathToNetcreate}/app-config/netcreate-config.js`);
+  shell.ShellString(script).to(`./${NC_CONFIG_PATH}/netcreate-config.js`);
 }
 
 function promiseServer(port) {
   shell.cd('netcreate-itest');
-  const server = require(`${pathToNetcreate}/brunch-server`);
+  const server = require(`./${NC_SERVER_PATH}/brunch-server`);
   return new Promise((resolve, reject) => {
     server({ port }, () => resolve());
   });
 }
 
-process.on("message", (data) => {
+process.on('message', data => {
   console.log(PRE);
-  console.log(PRE + "STARTING DB", data.db);
+  console.log(PRE, 'STARTING DB', data.db);
   console.log(PRE);
 
-  console.log(PRE + "1. Setting netcreate-config.js.");
+  console.log(PRE, '1. Setting netcreate-config.js.');
   writeConfig(data);
 
-  console.log(PRE + "2. Starting server");
+  console.log(PRE, '2. Starting server');
   startServer(data.port);
 });
 
 async function startServer(port) {
   await promiseServer(port);
   console.log(`${PRE} 3. Server started on port ${port}!`);
-  process.send("nc-start.js: Completed!");  
+  process.send('nc-launch-instance.js: Completed!');
 }
-
